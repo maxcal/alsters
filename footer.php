8267d52a68b8b8abd5369f5dfb78832ff414ebdf
Max Calabrese <max.calabrese@ymail.com>
2014-01-02 14:46:23 +0100

    copy'n'paste

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ */
+?>
+</div> <!-- /#container -->
+<div id="push"></div>
+<footer class="site-footer" role="contentinfo">
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span6 social-links">
+				<a class="btn" rel="tooltip" title="Se mer av oss!" target="_blank" href="http://facebook.com/elvenite"><i class="social-icon-facebook"></i> Facebook</a>
+				<a class="btn" rel="tooltip" title="Följ oss!" target="_blank" href="http://twitter.com/elvenite"><i class="social-icon-twitter"></i> Twitter</a>
+				<a class="btn" rel="tooltip" title="Se mer av oss!" target="_blank" href="http://www.linkedin.com/company/elvenite"><i class="social-icon-linkedin"></i> LinkedIn</a>
+				<a class="btn" rel="tooltip" title="Se mer av oss!" target="_blank" href="http://www.kiva.org/team/elvenite"><i class="social-icon-kiva"></i> Kiva</a>
+			</div>
+			<div class="span6 colophon">
+				<small class="pull-right">Copyright &copy; <?php echo date('Y'); ?> Elvenite AB</small>
+			</div>
+		</div>
+	</div>
+</footer><!-- #colophon -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
